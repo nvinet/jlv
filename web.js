81eb5cf623f3b4f476198fc5a3e8b41b3f7c5bd2
Nicolas Vinet <nvinet@gmail.com>
2015-09-12 17:34:40 +0100

    creating app

---
+++ web.js
@@ -0,0 +1,16 @@
+var gzippo = require('gzippo'),
+  express = require('express'),
+  morgan = require('morgan'),
+  path = require('path');
+
+var app = express();
+app.use(morgan('dev'));
+app.use(gzippo.staticGzip("dist"));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+app.set('port', process.env.PORT || 3000);
+var server = app.listen(app.get('port'), function() {
+  console.log('Express server listening on port ' + server.address().port);
+});
+
+module.exports = app;
