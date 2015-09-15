051e6f789eb5860e4003c873ee4e67fb36ba57e6
Nicolas Vinet <nvinet@gmail.com>
2015-09-15 22:40:51 +0100

    cleaning up gulp to remove useref

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,24 +3,37 @@ import gulp from 'gulp';
 import gulpLoadPlugins from 'gulp-load-plugins';
 import browserSync from 'browser-sync';
 import del from 'del';
-import {stream as wiredep} from 'wiredep';
 
 const $ = gulpLoadPlugins();
 const reload = browserSync.reload;
 
-gulp.task('styles', () => {
+gulp.task('sass', () => {
   return gulp.src('app/styles/*.scss')
     .pipe($.plumber())
-    .pipe($.sourcemaps.init())
     .pipe($.sass.sync({
       outputStyle: 'expanded',
       precision: 10,
       includePaths: ['.']
     }).on('error', $.sass.logError))
     .pipe($.autoprefixer({browsers: ['last 1 version']}))
-    .pipe($.sourcemaps.write())
     .pipe(gulp.dest('.tmp/styles'))
     .pipe(reload({stream: true}));
+});
+
+gulp.task("styles", () => {
+  gulp.task('scripts:vendor', () => {
+    return gulp.src([
+      'app/css/animate.css',
+      'app/css/fpslineicons.css',
+      'app/css/settings.css',
+      'app/css/swipebox.css',
+      'app/css/text-rotater.css',
+      'app/css/bootstrap.css',
+      'app/css/style.css'
+    ])
+      .pipe($.concat('main.css'))
+      .pipe(gulp.dest('public/styles'))
+  });
 });
 
 function lint(files, options) {
@@ -41,19 +54,42 @@ const testLintOptions = {
 gulp.task('lint', lint('app/scripts/**/*.js'));
 gulp.task('lint:test', lint('test/spec/**/*.js', testLintOptions));
 
-gulp.task('html', ['styles'], () => {
-  const assets = $.useref.assets({searchPath: ['.tmp', 'app', '.']});
-
-  return gulp.src('app/*.html')
-    .pipe(assets)
-    .pipe($.if('*.js', $.uglify()))
-    .pipe($.if('*.css', $.minifyCss({compatibility: '*'})))
-    .pipe(assets.restore())
-    .pipe($.useref())
-    .pipe($.if('*.html', $.minifyHtml({conditionals: true, loose: true})))
-    .pipe(gulp.dest('public'));
+gulp.task('scripts:vendor', () => {
+  return gulp.src([
+    'app/js/jquery.min.js'
+  ])
+    .pipe($.concat('vendor.js'))
+    .pipe(gulp.dest('./public/scripts/'))
 });
 
+gulp.task('scripts:plugins', () => {
+  return gulp.src([
+    'app/js/js/bootstrap.min.js',
+    'app/js/waypoints.min.js',
+    'app/js/jquery.counterup.min.js',
+    'app/js/jquery.textrotater.js',
+    'app/js/jquery.stellar.js',
+    'app/js/jquery.vopacity.js',
+    'app/js/wow.js',
+    'app/js/masonry.min.js',
+    'app/js/jquery.swipebox.js',
+    'app/js/jquery.themepunch.tools.min.js',
+    'app/js/jquery.themepunch.revolution.min.js'
+  ])
+    .pipe($.concat('plugins.js'))
+    .pipe(gulp.dest('./public/scripts/'))
+});
+
+gulp.task('scripts:main', () => {
+  return gulp.src([
+    'app/js/settings.js',
+    'app/js/scripts.js',
+    'app/scripts/main.js'
+  ])
+    .pipe($.concat('main.js'))
+    .pipe($.uglify())
+    .pipe(gulp.dest('./public/scripts/'))
+});
 
 gulp.task('images', () => {
   return gulp.src('app/images/**/*')
@@ -119,7 +155,6 @@ gulp.task('serve', ['styles', 'fonts'], () => {
   });
 
   gulp.watch([
-    'app/*.html',
     'app/scripts/**/*.js',
     'app/images/**/*',
     '.tmp/fonts/**/*'
@@ -127,7 +162,6 @@ gulp.task('serve', ['styles', 'fonts'], () => {
 
   gulp.watch('app/styles/**/*.scss', ['styles']);
   gulp.watch('app/fonts/**/*', ['fonts']);
-  gulp.watch('bower.json', ['wiredep', 'fonts']);
 });
 
 gulp.task('serve:public', () => {
@@ -157,23 +191,7 @@ gulp.task('serve:test', () => {
   gulp.watch('test/spec/**/*.js', ['lint:test']);
 });
 
-// inject bower components
-gulp.task('wiredep', () => {
-  gulp.src('app/styles/*.scss')
-    .pipe(wiredep({
-      ignorePath: /^(\.\.\/)+/
-    }))
-    .pipe(gulp.dest('app/styles'));
-
-  gulp.src('app/*.html')
-    .pipe(wiredep({
-      exclude: ['bootstrap-sass'],
-      ignorePath: /^(\.\.\/)*\.\./
-    }))
-    .pipe(gulp.dest('app'));
-});
-
-gulp.task('build', ['lint', 'html', 'images', 'ico','fonts', 'extras'], () => {
+gulp.task('build', ['lint', 'scripts:vendor', 'scripts:plugins', 'scripts:main', 'styles', 'images', 'ico','fonts', 'extras'], () => {
   return gulp.src('public/**/*').pipe($.size({title: 'build', gzip: true}));
 });
 
