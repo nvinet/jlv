96c0fd4e2de860177d5729a6bb9573dbd876034c
Nicolas Vinet <nvinet@gmail.com>
2015-09-16 17:18:41 +0100

    creating partials

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 hbs.localsAsTemplateData(app);
+hbs.registerPartials(__dirname + '/views/partials');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
