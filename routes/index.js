02b2778e0405040f14d123ee5f7d33020e9d7911
Nicolas Vinet <nvinet@gmail.com>
2015-10-04 18:31:57 +0100

    about and contact pages wording amendments.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'JLV Design Ltd' });
 });
 
+router.get('/contact', function(req, res, next){
+  res.render('contact', { title: 'JLV Design Ltd' });
+});
+
+router.get('/about', function(req, res, next){
+  res.render('about', { title: 'JLV Design Ltd' });
+});
+
 module.exports = router;
