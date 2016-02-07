0d811801dc6df69b57fb9c6e57a9b6f4830ac403
Nicolas Vinet <nvinet@gmail.com>
2016-02-07 18:08:44 +0000

    more es6 luv

---
+++ app/hbs/helpers.js
@@ -0,0 +1,13 @@
+import hbs from 'hbs';
+import path from 'path';
+import staticify from 'staticify';
+
+const $staticify = staticify(path.join(__dirname, 'public'));
+
+hbs.registerHelper('getVersionedPath', (path) => {
+  return $staticify.getVersionedPath(path);
+});
+
+hbs.registerHelper('toUpperCase', (str) => {
+  return str.toUpperCase();
+});
--- app/routes/index.js
+++ app/routes/index.js
@@ -5,26 +5,26 @@ import framesData from '../data/frames.json';
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res, next) => {
+router.get('/', (req, res) => {
   res.render('index', {
     title: 'JLV Design Ltd',
     data: homeData
   });
 });
 
-router.get('/contact', (req, res, next) => {
-  res.render('contact', { title: 'JLV Design Ltd' });
+router.get('/contact', (req, res) => {
+  res.render('contact', {title: 'JLV Design Ltd'});
 });
 
-router.get('/about', (req, res, next) => {
-  res.render('about', { title: 'JLV Design Ltd' });
+router.get('/about', (req, res) => {
+  res.render('about', {title: 'JLV Design Ltd'});
 });
 
-router.get('/stars', (req, res, next) => {
+router.get('/stars', (req, res) => {
   res.render('stars', {
     title: 'JLV Design Ltd',
     data: framesData
   });
 });
 
-module.exports = router;
+export default router;
