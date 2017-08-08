2a568b043ae5ac101f055587754d7568643940a1
Nicolas Vinet <nvinet@gmail.com>
2017-08-08 20:32:20 +0100

    updating project names adding new Muswell Hill family home fixing typos

--- app/routes/index.js
+++ app/routes/index.js
@@ -56,6 +56,10 @@ router.get('/projects/muswellHillFamily', (req, res) => {
   res.render('muswellHillFamily', {title: 'JLV Design Ltd', data: homeData});
 });
 
+router.get('/projects/familyHomeMuswellHill', (req, res) => {
+  res.render('firs', {title: 'JLV Design Ltd', data: homeData});
+});
+
 router.get('/projects/gardenOffice', (req, res) => {
   res.render('gardenOffice', {
     title: 'JLV Design Ltd',
